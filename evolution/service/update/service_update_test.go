716296cd032176f8c9dec648d28c6268f46e5a60
tadelesh <tadelesh.shi@live.cn>
2022-06-27 17:46:43 +0800

    add service evolution update test

--- evolution/service/update/service_update_test.go
+++ evolution/service/update/service_update_test.go
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-package initial
+package update
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
@@ -21,7 +22,7 @@ func newParamsClient() *ParamsClient {
 // DPGAddOptionalInput - Initially only has one required Query Parameter. After evolution, a new optional query parameter is added
 func TestDPGAddOptionalInput(t *testing.T) {
 	client := newParamsClient()
-	resp, err := client.GetRequired(context.Background(), "requiredParam", nil)
+	resp, err := client.GetRequired(context.Background(), "requiredParam", &ParamsClientGetRequiredOptions{NewParameter: to.Ptr("newOptionalParam")})
 	require.NoError(t, err)
 	require.NotNil(t, resp.Interface)
 }
@@ -29,14 +30,14 @@ func TestDPGAddOptionalInput(t *testing.T) {
 // DPGAddOptionalInput_NoParams - Initially has no query parameters. After evolution, a new optional query parameter is added
 func TestDPGAddOptionalInputNoParams(t *testing.T) {
 	client := newParamsClient()
-	_, err := client.HeadNoParams(context.Background(), nil)
+	_, err := client.HeadNoParams(context.Background(), &ParamsClientHeadNoParamsOptions{NewParameter: to.Ptr("newOptionalParam")})
 	require.NoError(t, err)
 }
 
 // DPGAddOptionalInput_RequiredOptionalParam - Initially has one required query parameter and one optional query parameter.  After evolution, a new optional query parameter is added
 func TestDPGAddOptionalInputRequiredOptionalParam(t *testing.T) {
 	client := newParamsClient()
-	resp, err := client.PutRequiredOptional(context.Background(), "requiredParam", &ParamsClientPutRequiredOptionalOptions{OptionalParam: to.Ptr("optionalParam")})
+	resp, err := client.PutRequiredOptional(context.Background(), "requiredParam", &ParamsClientPutRequiredOptionalOptions{OptionalParam: to.Ptr("optionalParam"), NewParameter: to.Ptr("newOptionalParam")})
 	require.NoError(t, err)
 	require.NotNil(t, resp.Interface)
 }
@@ -44,14 +45,37 @@ func TestDPGAddOptionalInputRequiredOptionalParam(t *testing.T) {
 // DPGAddOptionalInput_OptionalParam - Initially has one optional query parameter. After evolution, a new optional query parameter is added
 func TestDPGAddOptionalInputOptionalParam(t *testing.T) {
 	client := newParamsClient()
-	resp, err := client.GetOptional(context.Background(), &ParamsClientGetOptionalOptions{OptionalParam: to.Ptr("optionalParam")})
+	resp, err := client.GetOptional(context.Background(), &ParamsClientGetOptionalOptions{OptionalParam: to.Ptr("optionalParam"), NewParameter: to.Ptr("newOptionalParam")})
+	require.NoError(t, err)
+	require.NotNil(t, resp.Interface)
+}
+
+// DPGAddNewOperation - Initially the path exists but there is no delete method. After evolution this is a new method in a known path
+func TestDPGAddNewOperation(t *testing.T) {
+	client := newParamsClient()
+	_, err := client.DeleteParameters(context.Background(), nil)
+	require.NoError(t, err)
+}
+
+// DPGAddNewPath - Initiallty neither path or method exist for this operation. After evolution, this is a new method in a new path
+func TestDPGAddNewPath(t *testing.T) {
+	client := newParamsClient()
+	resp, err := client.GetNewOperation(context.Background(), nil)
 	require.NoError(t, err)
 	require.NotNil(t, resp.Interface)
 }
 
 // DPGNewBodyType - Initial version is JSON only, and now supports also JPEG
 func TestDPGNewBodyType(t *testing.T) {
+	// breaking
 	client := newParamsClient()
-	_, err := client.PostParameters(context.Background(), PostInput{URL: to.Ptr("http://example.org/myimage.jpeg")}, nil)
+	_, err := client.PostParametersWithJSON(context.Background(), PostInput{URL: to.Ptr("http://example.org/myimage.jpeg")}, nil)
+	require.NoError(t, err)
+
+	file, err := os.OpenFile("test.jpg", os.O_RDONLY, os.ModeAppend)
+	require.NoError(t, err)
+	_, err = client.PostParameters(context.Background(), file, nil)
 	require.NoError(t, err)
 }
+
+// DPGGlassBreaker - Not support
